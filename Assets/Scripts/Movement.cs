c076b0e37b74816e2f77afb0ba37a2e773f5b9f4
EricChurches <ec2015@outlook.com>
2022-06-30 01:57:24 +1200

    created prototype  levels

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody _rb;// WE PUT rigidbody into a variable
+
+    [SerializeField] float mainThrust = 1000f;
+
+    [SerializeField] private float rotationThrust = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();// the value of the variable
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       ProcessThrust();// the methods
+       ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rb.AddRelativeForce(Vector3.up * (mainThrust * Time.deltaTime));
+        }
+
+        
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        _rb.freezeRotation = true;//freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * (rotationThisFrame * Time.deltaTime));
+        _rb.freezeRotation = false;// back to the physics machine
+    }
+}
